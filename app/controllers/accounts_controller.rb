f49f9a0bde1bf05733a90306a0afe788b60ea294
Dong Ju Kang <celica212@company100.com>
2015-05-27 01:10:06 +0900

    Implement sign-in action.
    Currently, sign-in action acts as sign-up action.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,16 @@
 class AccountsController < ApplicationController
+  def create
+    @account = Account.new(account_params)
+    @account.save
+    redirect_to @account
+  end
+
+  private
+  def account_params
+    params.require(:account).permit(:email, :password)
+  end
+
+  def show
+    @account = Account.find(params[:id])
+  end
 end
