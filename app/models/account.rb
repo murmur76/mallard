9fa6bdcba797997a913f1c02c3013781db24d166
Dong Ju Kang <celica212@company100.com>
2015-06-26 15:35:04 +0900

    Add basic validation and test for account model.

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,3 @@
 class Account < ActiveRecord::Base
+  validates :email, :password,  presence: true
 end
--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class AccountTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save account without fields" do
+    account = Account.new
+    assert_not account.save, "Saved the account without fields"
+  end
 end
