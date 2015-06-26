f9da6b443f8ab0372510d19dd5197338e6ab333f
Dong Ju Kang <celica212@company100.com>
2015-06-26 15:35:32 +0900

    Add basic test cases for account controller.

--- test/controllers/accounts_controller_test.rb
+++ test/controllers/accounts_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class AccountsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should create account" do
+    assert_difference('Account.count') do
+      post :create, account: accounts(:user1).serializable_hash()
+    end
+    assert_redirected_to account_path(assigns(:account))
+  end
 end
